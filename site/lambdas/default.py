4b6cb7c67865c320695cf441f8278f3c31c73e9f
priyatam <priyatam@gmail.com>
2013-09-03 10:55:48 -0400

    deleted site and moved to another project

--- site/lambdas/default.py
+++
@@ -1,8 +0,0 @@
-import pystache
-
-def hello_world(text):
-    return "Hello %s from Frozen Pie" % text
-
-def all_posts(text):
-    renderer = pystache.Renderer(file_encoding="utf-8", string_encoding="utf-8")
-    return renderer.render(text, {"posts": contents })
